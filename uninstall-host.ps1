505790723f2dc5a007ac4da47acf9d2e8a17eab5
Lmjfemc <mj0608.ie.work@gmail.com>
2026-09-17 23:34:55 +0900

    Remove superseded PowerShell host uninstaller

--- uninstall-host.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$key = 'HKCU:\Software\Google\Chrome\NativeMessagingHosts\com.netflixlan.remote'
-if (Test-Path $key) { Remove-Item -LiteralPath $key }
-Write-Output 'Native host registration removed. Project files are preserved.'
